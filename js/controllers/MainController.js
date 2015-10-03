c1bfbff0133623c1dc31290541df0aa91ce509a1
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-03 15:06:25 -0500

    Add Bootstrap and display cat information

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,24 @@
 app.controller('MainController', ['$scope', function($scope) {
-  
+  $scope.title = "catstagram";
+
+  $scope.cats = [
+    {
+      username: "@spoons",
+      name: "Spooky",
+      age: 13,
+      breed: "tuxedo",
+      likes: 0
+    },
+    {
+      username: "@beal",
+      name: "Bill",
+      age: 11,
+      breed: "tuxedo",
+      likes: 0
+    }
+  ];
+
+  $scope.addOne = function(index) {
+    $scope.cats[index].likes += 1;
+  };
 }]);
