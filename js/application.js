0e4a41557c569750da399c23cd8a43c83c8189d4
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-04 20:31:15 -0500

    Configure ui-router

--- js/application.js
+++ js/application.js
@@ -1,1 +1,18 @@
 var app = angular.module("catstagram", ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainController'
+    });
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsController'
+    });
+
+  $urlRouterProvider.otherwise('home');
+
+}]);
