fb19a3b69280f01a73f570dab95ee1a9382827dd
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-04 19:03:22 -0500

    Add posts service and include ui-router library

--- js/application.js
+++ js/application.js
@@ -1,1 +1,1 @@
-var app = angular.module("catstagram", []);
+var app = angular.module("catstagram", ['ui.router']);
---
+++ js/services/posts.js
@@ -0,0 +1,6 @@
+app.factory('posts', [function() {
+  var postObject = {
+    posts: []
+  };
+  return postObject;
+}]);
